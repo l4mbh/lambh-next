f38a860c9d2fc34206d9204a73bfdfc20db99e4a
l4mbh <lambh.1998@gmail.com>
2026-02-24 17:21:45 +0700

    chore: delete Notion API test files

--- test-notion-official.ts
+++
@@ -1,8 +0,0 @@
-const { Client } = require('@notionhq/client');
-const dotenv = require('dotenv');
-dotenv.config();
-
-console.log("Client:", Client);
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
-console.log("notion keys:", Object.keys(notion));
-console.log("notion.databases:", notion.databases);
